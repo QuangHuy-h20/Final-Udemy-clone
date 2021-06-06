b23faecc6d37c8014cb3411a05451e19f4baf4a2
Quang Huy <quanghuy.h20@gmail.com>
2021-06-06 15:50:58 +0700

    Update project

--- src/auth/AdminRoute.js
+++ src/auth/AdminRoute.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -17,23 +16,3 @@ export default function AdminRoute({ children, ...props }) {
   }
   return <Route {...props}>{children}</Route>;
 }
-=======
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
-
-export default function AdminRoute({ children, ...props }) {
-  const { userInfo } = useSelector((state) => state.auth);
-
-  //Not login
-  if (!userInfo) {
-    return <Redirect to={`/login?redirectTo=${props.path}`} />;
-  }
-
-  //Login but not admin role
-  if (userInfo.maLoaiNguoiDung !== "GV") {
-    return <Redirect to="/" />;
-  }
-  return <Route {...props}>{children}</Route>;
-}
->>>>>>> tu
--- src/pages/AdminCourses/index.js
+++ src/pages/AdminCourses/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 const AdminCourses = () => {
@@ -10,31 +9,3 @@ const AdminCourses = () => {
 }
 
 export default AdminCourses
-=======
-<<<<<<< HEAD
-import React from 'react'
-
-const AdminCourses = () => {
-    return (
-        <div>
-            <h1>AdminCourses</h1>
-        </div>
-    )
-}
-
-export default AdminCourses
-=======
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-
-
-const AdminCourses = () => {
-
-  return (
-    <h1>Admin Courses</h1>  
-  );
-};
-
-export default AdminCourses;
->>>>>>> tu
->>>>>>> 04c883a45ad57d0df8dd2142b9640b07b2233bae
