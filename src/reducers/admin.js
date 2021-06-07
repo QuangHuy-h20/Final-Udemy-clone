7334f548e88d8a9bc689197fe426490a428a755a
Quang Huy <quanghuy.h20@gmail.com>
2021-06-07 08:57:42 +0700

    Update Sidebar component

--- src/reducers/admin.js
+++ src/reducers/admin.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-import {
-    GET_USER_LIST_REQUEST,
-    GET_USER_LIST_SUCCESS,
-    GET_USER_LIST_FAILURE
-} from "../constants/admin"
-
-const initialState = {
-    userList: [],
-    isLoading: false,
-    error: null,
-};
-
-function adminReducer(state = initialState,action){
-    switch(action.type){
-        case GET_USER_LIST_REQUEST:{
-            return {...state, 
-                isLoading:true, 
-                error: null};
-        }
-        case GET_USER_LIST_SUCCESS:{
-            return {...state, 
-                isLoading:false, 
-                userList: action.payload.data,
-            }
-        }
-        case GET_USER_LIST_FAILURE: {
-            return{
-                ...state,
-                isLoading:true,
-                error:action.payload.error,
-            }
-
-        }
-        default:{
-            return state
-        }
-    }
-}
-=======
 import {
     GET_USER_LIST_REQUEST,
     GET_USER_LIST_SUCCESS,
@@ -98,5 +58,4 @@ function adminReducer(state = initialState,action){
         }
     }
 }
->>>>>>> tu
 export default adminReducer
